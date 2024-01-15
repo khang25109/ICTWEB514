1f1e1174d97f5edf69ebdc62c26c7b709ef3c8c4
khang25109 <111103391+khang25109@users.noreply.github.com>
2024-01-15 11:55:16 +1100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+
+<div class="header">
+    <div class="right">REAL HOME</div>
+    <div></div>
+    <div class="left">
+        <div><button>SIGN UP</button></div>
+        <div><button>LOG IN</button></div>
+    </div>
+
+</div>
+
+<div class="topnav">
+  <a href="#">BUY</a>
+  <a href="#">SELL</a>
+  <a href="#">RENT</a>
+  <a href="#">MORTGAGE</a>
+  <a href="#">FIND AN AGENT</a>
+  <a href="#">TOOLS</a>
+  <a href="#">ABOUT</a>
+  <a href="#">BLOG</a>
+  <a href="#">CONTACT</a>
+  <a href="#" style="float:right">Link</a>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <div class="title">
+        <h2>Sweet Home</h2>
+        <h5>Australia Luxury Real Estate, Dec 7, 2023</h5>
+        <div class="img1">
+          <img src="images/house1.webp" alt="">
+        </div>
+      </div>
+      <div class="text">
+      <p>DESCRIPTION</p>
+        <p>Step into a world of unparalleled luxury and sophistication. Discover this magnificent resort-inspired 
+           home which has recently undergone a full renovation. Designed by renowned and multi award winning
+           residential architects Sumich Chaplin along with beautiful landscaping crafted by Suzanne Turley Landscapes. 
+           This extraordinary residence has remained one of the best kept secrets in this coveted neighborhood and it’s time to unveil its opulence. 
+           This residence isn’t just a home, it’s a true work of art. From the moment you enter the welcoming foyer, 
+           you’ll be captivated by the abundance of space and architectural design. Every inch of this residence exudes excellence, with countless WOW factors to discover. 
+           From the library to the multiple living and entertaining areas, you’ll be enchanted by the attention to detail and craftsmanship that has poured into this home re-creation. 
+           For those with an active lifestyle, this home boasts a state-of-the-art gym and a champion-size squash court, ensuring that you can stay fit and have fun within 
+           the comfort of your own space. And when it’s time to unwind, step outside to your outdoor oasis – complete with a heated pool, an outdoor spa and an all-year-round cabana,
+           equipped with built-in barbeque, ice machine, wet bar and fireplace. The list of exceptional features doesn’t stop here. Exquisite finishes grace every corner of this home.
+           Showcasing high level of workmanship that is truly second to none. While the house boasts luxury and elegance, it also envelops you in a warm and inviting atmosphere that makes
+           it feel like a home. And for those who appreciate their automotive treasures,
+           a 4-car garage awaits, with additional space for three more cars inside the gated compound</p>
+        <p>$1.920.000 USD</p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="title">
+        <h2>Green Home</h2>
+        <h5>DETACHED HOUSE IN AUCKLAND, Dec 29, 2022</h5>
+        <div class="img1">
+          <img src="images/house1.webp" alt="">
+        </div>
+      </div>
+      <div class="text">
+        <p>DESCRIPTION</p>
+        <p>Step into a world of unparalleled luxury and sophistication. Discover this magnificent resort-inspired 
+           home which has recently undergone a full renovation. Designed by renowned and multi award winning
+           residential architects Sumich Chaplin along with beautiful landscaping crafted by Suzanne Turley Landscapes. 
+           This extraordinary residence has remained one of the best kept secrets in this coveted neighborhood and it’s time to unveil its opulence. 
+           This residence isn’t just a home, it’s a true work of art. From the moment you enter the welcoming foyer, 
+           you’ll be captivated by the abundance of space and architectural design. Every inch of this residence exudes excellence, with countless WOW factors to discover. 
+           From the library to the multiple living and entertaining areas, you’ll be enchanted by the attention to detail and craftsmanship that has poured into this home re-creation. 
+           For those with an active lifestyle, this home boasts a state-of-the-art gym and a champion-size squash court, ensuring that you can stay fit and have fun within 
+           the comfort of your own space. And when it’s time to unwind, step outside to your outdoor oasis – complete with a heated pool, an outdoor spa and an all-year-round cabana,
+           equipped with built-in barbeque, ice machine, wet bar and fireplace. The list of exceptional features doesn’t stop here. Exquisite finishes grace every corner of this home.
+           Showcasing high level of workmanship that is truly second to none. While the house boasts luxury and elegance, it also envelops you in a warm and inviting atmosphere that makes
+           it feel like a home. And for those who appreciate their automotive treasures,
+           a 4-car garage awaits, with additional space for three more cars inside the gated compound</p>
+        <p>$1.620.000 USD</p>
+      </div>
+  </div>
+  
+   
+  </div>
+</div>
+<div class="main2">
+  <p class="p1">LET YOUR DREAMS COME TRUE</p>
+  <p class="p2">BUILDING HAPPY HOMES, CREATING HEALTHY LIVING SPACES</p>
+  <p class="p3">All the Lorem IPsum generators on the All the Lorem IPsum generators on the All the Lorem IPsum generators on the All the Lorem IPsum generators on the
+  All the generators on the All the Lorem IPsum generators on Lorem IPsum generators on the All the Lorem IPsum generators on the All the Lorem IPsum generators on the
+  All the Lorem IPsum generators on the All the Lorem IPsum generators on generators on the All the Lorem IPsum generators on the All the Lorem IPsum generators on the
+  </p>
+  
+</div>
+
+<div class="footer">
+  <p class="footerP">Minh Khang Nguyen &copy; 2023</p>
+</div>
+
+</body>
+</html>
